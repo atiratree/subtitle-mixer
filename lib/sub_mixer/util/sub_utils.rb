ca1f055bdc9026e3376d1ded827c91b8462d04ec
suomiy <suomiy@gmail.com>
2018-01-27 00:58:46 +0100

    refactor sub utils

--- lib/sub_mixer/util/sub_utils.rb
+++ lib/sub_mixer/util/sub_utils.rb
@@ -6,9 +6,9 @@ module SubMixer
         [:srt, :ssa, :ass]
       end
 
-      def detect_format(string)
-        # matches ssa/ass
-        captures = string.match(/\[Script Info\].*ScriptType:\s*v(?<version>[0-9\.]+)(?<plus>\+?)/m)
+      def detect_ssa_or_ass(version)
+        version = version.strip
+        captures = version.match(/v(?<version>[0-9\.]+)(?<plus>\+?)/)
         if captures
           version = captures[:version].to_f
           is_plus = captures[:plus] == '+'
@@ -19,9 +19,19 @@ module SubMixer
             :ssa
           end
         else
-          # matches srt
-          if string.match(/[^[[:space:]]]+ -+> [^[[:space:]]]+ ?(X1:\d+ X2:\d+ Y1:\d+ Y2:\d+)?/m)
-            :srt
+          raise 'Unknown format'
+        end
+      end
+
+      def detect_format(string)
+        # matches srt
+        if string.match(/[^[[:space:]]]+ -+> [^[[:space:]]]+ ?(X1:\d+ X2:\d+ Y1:\d+ Y2:\d+)?/m)
+          :srt
+        else
+          # matches ssa/ass
+          captures = string.match(/\[Script Info\].*ScriptType:\s*(?<version>[^\n]+)\s*\n/m)
+          if captures
+            detect_ssa_or_ass captures[:version]
           else
             raise 'Unknown format'
           end
