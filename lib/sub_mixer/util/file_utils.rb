603669c1e565d63f967a8eb0e778e157239849b1
suomiy <suomiy@gmail.com>
2018-01-26 18:24:06 +0100

    remove BOM from UTF files

--- lib/sub_mixer/util/file_utils.rb
+++ lib/sub_mixer/util/file_utils.rb
@@ -2,7 +2,15 @@ require 'charlock_holmes'
 
 module SubMixer
   module FileUtils
+
     class << self
+      BOM_LIST_HEX = {
+          Encoding::UTF_8 => "\xEF\xBB\xBF", #"\uEFBBBF"
+          Encoding::UTF_16BE => "\xFE\xFF", #"\uFEFF",
+          Encoding::UTF_16LE => "\xFF\xFE",
+          Encoding::UTF_32BE => "\x00\x00\xFE\xFF",
+          Encoding::UTF_32LE => "\xFE\xFF\x00\x00",
+      }
 
       def read(filename, encoding=nil)
         begin
@@ -21,6 +29,8 @@ module SubMixer
         rescue
           raise "Failure while setting encoding for #{filename}"
         end
+
+        content = remove_bom(content)
 
         begin
           content.encode('UTF-8')
@@ -42,6 +52,16 @@ module SubMixer
       end
 
       private
+
+      def remove_bom(content)
+        if bom = BOM_LIST_HEX[content.encoding]
+          bom_size = bom.bytesize
+          if content.bytes[0...bom_size] == bom.bytes # check it is indeed BOM
+            return content.byteslice(bom_size...content.bytesize) # remove it
+          end
+        end
+        content
+      end
 
       def with_extension(path, extension)
         path = path.strip
